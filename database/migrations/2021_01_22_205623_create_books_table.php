fa71c1e731036f80119646da8bd039f99670b430
Flormarys <flormarysdiaz@gmail.com>
2021-01-22 19:42:19 -0300

    creating migrations

---
+++ database/migrations/2021_01_22_205623_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('url');
+            $table->foreignId('subject_id')->constrained('subject');
+            $table->string('language');
+            $table->integer('word_count');
+            $table->boolean('is_original');
+            $table->string('based_on')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
